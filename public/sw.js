137a5eefee153678d1e2276d44575de888ab3183
MAHDTech <MAHDTech@saltlabs.tech>
2024-12-10 18:04:37 +1100

    fix: PWA caching

--- public/sw.js
+++ public/sw.js
@@ -51,7 +51,24 @@ const urlsToCache = [
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return Promise.all(
+        urlsToCache.map((url) => {
+          return fetch(url)
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error(
+                  `Failed to fetch ${url}: ${response.statusText}`
+                );
+              }
+              return cache.put(url, response);
+            })
+            .catch((error) => {
+              console.error(`Failed to cache ${url}:`, error);
+            });
+        })
+      );
+    })
   );
 });
 
